06db1bc2196864126cf1749a611a72d016b9b472
Roct <roct@Noct.(none)>
2013-07-07 16:08:59 +1000

    added app

---
+++ js/all.js
@@ -0,0 +1,7 @@
+---
+---
+{% include js/jquery.min.js %}
+{% include js/underscore.min.js %}
+{% include js/bootstrap/bootstrap.min.js %}
+{% include js/bootstrap/google-code-prettify/prettify.js %}
+{% include js/jquery.githubwidget.min.js %}
