39deb28abd97daf36607fde0fd3755b993497826
Roct <roct@Noct.(none)>
2013-07-14 16:32:51 +1000

    fixed js load order

--- js/all.js
+++ js/all.js
@@ -5,6 +5,6 @@
 {% include js/bootstrap/bootstrap.min.js %}
 {% include js/bootstrap/google-code-prettify/prettify.js %}
 {% include js/jquery.githubwidget.min.js %}
+{% include vendor/elastislide/modernizr.custom.17475.js %}
 {% include vendor/elastislide/jquery.elastislide.js %}
 {% include vendor/elastislide/jquerypp.custom.js %}
-{% include vendor/elastislide/modernizr.custom.17475.js %}
